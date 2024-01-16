16578582996f7bed0f9473a2410e92c55271abfe
rajshubham1999 <rajshubham834@gmail.com>
2024-01-16 12:33:30 +0530

    first commit

---
+++ src/customer/Components/Carousel/HomeCaroselData.js
@@ -0,0 +1,24 @@
+export const homeCarouselData=[
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/2/_/2_10.jpg",
+        path:"/women/clothing/women_saree"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/_/1_6.jpg",
+        path:"/women/clothing/lengha_choli"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/5/_/5_8.jpg",
+        path:"/women/clothing/women_dress"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/4/_/4_9.jpg",
+        path:"/women/clothing/women_dress"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/3/_/3_10.jpg",
+        path:"/women/clothing/women_dress"
+    }
+
+]
+
